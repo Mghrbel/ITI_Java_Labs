db57f0de29c330a0a3226b347502078cdbd05362
Mohamed Ebrahim Elmghrbel <64231737+Mghrbel@users.noreply.github.com>
2021-06-19 14:25:05 +0200

    Editing try body to avoid exceptions

--- Day 02/01_Cities_Countries_Map/src/cities_countries_map/CityCSVDAO.java
+++ Day 02/01_Cities_Countries_Map/src/cities_countries_map/CityCSVDAO.java
@@ -1,12 +1,10 @@
-package cities_countries_map;
+package MYPACKAGE;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class CityCSVDAO {
     String Path;
@@ -22,16 +20,16 @@ public class CityCSVDAO {
         try {
             File citiesFile = new File(Path);
             List<String> lines = Files.readAllLines(citiesFile.toPath());
-        } catch (IOException ex) {
-            Logger.getLogger(CityCSVDAO.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        for(int cityIndex=1 ; cityIndex<lines.size() ; cityIndex++){
-            String line = lines.get(cityIndex);
-            String[] fields = line.split(",");
-            if (fields.length == 5){
-                City city = new City(Integer.parseInt(fields[0]), fields[1], fields[2], fields[3], Double.parseDouble(fields[4]));
-                cities.add(city);
+            for (int cityIndex = 1; cityIndex < lines.size(); cityIndex++) {
+                String line = lines.get(cityIndex);
+                String[] fields = line.split(",");
+                if (fields.length == 5) {
+                    City city = new City(Integer.parseInt(fields[0]), fields[1], fields[2], fields[3], Double.parseDouble(fields[4]));
+                    cities.add(city);
+                }
             }
+        } catch (IOException ex) {
+            System.out.println("File not found");
         }
         return cities;
     }
